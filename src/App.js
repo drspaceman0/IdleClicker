286f6f4970c15f4a96f0e6f5e26597423f9a2b5f
Eric <ericmrsh9@gmail.com>
2021-11-08 16:43:04 -0800

    More changes

--- src/App.js
+++ src/App.js
@@ -7,28 +7,26 @@ class App extends Component {
     this.state = {
       intervalAmount: 1000,
       totalPoints: 0,
-      totalHelpers_1: 0, 
+      totalHelpers_1: 0,
     };
- 
   }
 
   componentDidMount() {
     // set interval
-    this.timerID = setInterval(() => this.tick(),1000); 
+    this.timerID = setInterval(() => this.tick(), 1000);
   }
 
   componentWillUnmount() {
-      clearInterval(this.timerID);
+    clearInterval(this.timerID);
   }
 
-
-  tick() { 
-      console.log("tick! helpers add: " + this.state.totalHelpers_1); 
-      // helper (tier 1)
-      if(this.state.totalHelpers_1 > 0) {
-        let addend = this.state.totalHelpers_1;
-        this.setState({totalPoints: this.state.totalPoints + addend});
-      }
+  tick() {
+    console.log("tick! helpers add: " + this.state.totalHelpers_1);
+    // helper (tier 1)
+    if (this.state.totalHelpers_1 > 0) {
+      let addend = this.state.totalHelpers_1;
+      this.setState({ totalPoints: this.state.totalPoints + addend });
+    }
   }
 
   incrementTotalPoints() {
@@ -43,20 +41,21 @@ class App extends Component {
   render() {
     return (
       <div className="main">
-        <header>Idle Clicker</header>
+      <header>Stu&apos;s Egg Mistake</header>
         <div>
-          <span>Total Points: {this.state.totalPoints}</span>
-        </div>
+        <span className="value-label">
+        Eggs: {this.state.totalPoints}  
+        </span>
+        <button className="value-button" onClick={() => this.incrementTotalPoints()}>+1 Egg</button></div> 
         <div>
-          <span>Helpers: {this.state.totalHelpers_1}</span>
-        </div>
-        <div>
-          <button onClick={() => this.incrementTotalPoints()}>
-            +1 Points
-          </button>
-          <button onClick={() => this.incrementTotalHelpers_1()}>
+        <span className="value-label">Helpers: {this.state.totalHelpers_1}</span>
+          <button className="value-button" onClick={() => this.incrementTotalHelpers_1()}>
             +1 Helpers (Tier 1)
           </button>
+        </div>
+        <div className="buttons">
+         
+          
         </div>
       </div>
     );
---
+++ src/Helper.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+const Helper = () => {
+    const [incrementAmount, setIncrementAmount] = useState(0);
+    const [costAmount, setCostAmount] = useState(0);
+
+    return (
+        <div></div>
+    );
+}
+
+
+export default Helper;
